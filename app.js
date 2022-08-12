edba3480b5df41ae0954a2442afe8aeb1cbf1ef0
sahil937 <meharsahil207@gmail.com>
2022-08-12 13:46:47 -0500

    new pacakges are installed

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
+
 
 const port = process.env.PORT || 8081;
 
